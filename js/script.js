7de1e69c19f52433dda12b01307189430315d8b3
Kenny-Le-281 <99355017+Kenny-Le-281@users.noreply.github.com>
2022-03-10 18:29:56 +0000

    added button

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Created by: Kenny Le
+ * Created on: Mar 2022
+ * This file contains the JS functions for index.html 
+*/
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  document.getElementById('multiply-math').innerHTML = '<p>The area is: ? ' + (5 * 3) + '</p>'
+  document.getElementById('add-math').innerHTML = '<p>The perimeter is: ? ' + (5 + 5 + 3 + 3) + '</p>'
